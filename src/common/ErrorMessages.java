7489d88039c93e367a89bb027ca51eb46b5cf6b8
hackertyper <hackertyper@users.noreply.github.com>
2016-12-07 13:31:02 +0100

    Implementierungen

---
+++ src/common/ErrorMessages.java
@@ -0,0 +1,7 @@
+package common;
+
+public class ErrorMessages {
+	
+	public static final String invalidStocknumbers = "Minimum stock number must be less or equal maximum!";
+	public static String reserveNegative = "Reserve must not be negative!";
+}
