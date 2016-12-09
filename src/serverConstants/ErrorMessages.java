2916183715b745048eab63d931b9635f78a7d23f
Jennifer Bathke <bathke@his.de>
2016-12-09 13:03:00 +0100

    Translatet error messages

--- src/serverConstants/ErrorMessages.java
+++ src/serverConstants/ErrorMessages.java
@@ -2,13 +2,13 @@ package serverConstants;
 
 public class ErrorMessages {
 
-	public static final String InsufficientStock = "Not enough articles in stock to process order. Please lower your article amount or choose to preorder.";
-	public static final String InsufficientFunds = "Not enough money on account balance. Please deposit the difference before order.";
-	public static final String DoubleUsername = "AccountName is already in use. Please choose another name.";
-	public static final String FirstCheckOut = "Cart needs to be checked out before ordering.";
-	public static final String NotArrived = "Order has yet to arrive. Please wait.";
-	public static final String invalidStocknumbers = "Minimum stock number must be less or equal maximum!";
-	public static final String reserveNegative = "Reserve must not be negative!";
+	public static final String InsufficientStock = "Nicht ausreichend Artikel im Lager. Bitte reduzieren Sie die Artikelmenge.";
+	public static final String InsufficientFunds = "Sie haben nicht ausreichend Guthaben auf Ihrem Konto. Bitte zahlen Sie vor dem Bestellen etwas ein.";
+	public static final String DoubleUsername = "Der von Ihnen gewählte Name ist bereits vergeben. Bitte wählen Sie einen anderen.";
+	public static final String FirstCheckOut = "Bitte gehen Sie erst zur Kasse bevor Sie bestellen.";
+	public static final String NotArrived = "Ihre Bestellung ist noch nicht angekommen. Bitte warten Sie.";
+	public static final String invalidStocknumbers = "Minimallagerbestand muss weniger oder gleich dem Maximallagerbestand sein!";
+	public static final String reserveNegative = "Achten Sie darauf das Reserve nicht negativ ist!";
 
 	public ErrorMessages() {}
 	
